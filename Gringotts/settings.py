24f50b7805e5120c17baa3067f7dda29bf882624
Kaushik Shridhar <shridharkaushik1@gmail.com>
2021-06-14 22:10:08 +0530

    modified settings.py

--- Gringotts/settings.py
+++ Gringotts/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a&ze##tjthk8=50tu4%d8p+0+=dlmjfpemykh8khtuals3(0%r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.6', 'gringotts-spoof.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'gringotts-spoof.herokuapp.com']
 
 
 # Application definition
